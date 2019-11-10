bcfc6dfd095436b87987574ee06a13deb93592b5
Alexander Kuvaev <alexander@kuvaev.me>
2019-11-10 16:21:02 +0300

    Add contest info and contestants pages

--- main.py
+++ main.py
@@ -12,11 +12,28 @@ from flask import Flask, request, redirect, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.abspath('cf-challange.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.abspath('cf-challange.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # SQLALCHEMY_TRACK_MODIFICATIONS deprecated
 #app.config['SERVER_NAME'] = # NAME
 #app.config['SERVER_PORT'] = # Port
 db = SQLAlchemy(app)
+
+class Contestant(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    handle = Column(String(10000), unique=True)
+    name = Column(String(10000))
+
+    def __init__(self, handle, name):
+        self.handle = handle
+        self.name = name
+
+class Contest(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(10000))
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
 
 def print_err(s):
     print(s, file=sys.stderr)
@@ -78,24 +95,86 @@ def get_data(cfdata):
     result += '</table>'
     return result
 
+def get_contestants_table():
+    result = ''
+    result += '<table class="table">'
+    result += '<tr>'
+    result += '<th scope="col">Участник</th>' 
+    result += '<th scope="col">CF Handle</th>'
+    result += '</tr>'
+    contestants = Contestant.query.all()
+    for contestant in contestants:
+        result += '<tr>'
+        result += '<td>' + contestant.name + '</td>'
+        result += '<td><a href="https://codeforces.com/profile/' +\
+            contestant.handle + '">' + contestant.handle + '</td>'
+        result += '</tr>'
+    result += '</table>'
+    return result
+
+@app.route('/contestants', methods = ["GET", "POST"])
+def contestants():
+    if request.method == 'POST':
+        handle = request.form['handle']
+        name = request.form['name']
+        contestant = Contestant(handle, name)
+        db.session.add(contestant)
+        db.session.commit()
+
+    
+    with open('include/contestants.html', 'r', encoding="utf-8") as page:
+        data=page.read()
+    data = data.replace('%CONTESTANTS%', get_contestants_table())
+    return data 
+
+@app.route('/contest_info/<int:contestId>', methods = ["GET", "POST"])
+def contest_info(contestId):
+    rnd = str(random.randrange(100000, 1000000))
+    tm = str(int(time.time()))
+    params = ['apiKey=' + secret.cfpubkey, 'contestId=' + str(contestId), 'time=' + tm]
+    req = '/contest.standings?'
+    hs = rnd + req + '&'.join(params) + '#' + secret.cfsecretkey
+    hs = hs.encode('utf-8')
+    hs = str(hashlib.sha512(hs).hexdigest())
+    ref = "https://codeforces.com/api" + req + "&".join(params)
+    ref += '&apiSig=' + rnd + hs
+    response = requests.get(ref)
+    cfdata = get_data(response.json()['result']) 
+
+    with open('include/contest_info.html', 'r', encoding="utf-8") as page:
+        data=page.read()
+    data = data.replace('%CF_DATA%', cfdata)
+    return data 
+
+
+def get_contest_table():
+    result = ''
+    result += '<table class="table">'
+    result += '<tr>'
+    result += '<th scope="col">Результаты</th>'
+    result += '<th scope="col">Результаты на CF</th>'
+    result += '</tr>'
+    contests = Contest.query.all()
+    for contest in contests:
+        result += '<tr>'
+        result += '<td><a href="contest_info/' +\
+            str(contest.id) + '">' + contest.name + '</td>'
+        result += '<td>https://codeforces.com/group/U9XUJaQnsj/contest/' +\
+            str(contest.id) + '</td>'
+        result += '</tr>'
+    result += '</table>'
+    return result
+
 @app.route('/', methods = ["GET", "POST"])
 def root():
-    cfdata = ""
     if request.method == 'POST':
-        contestId = request.form['contestId']
-        rnd = str(random.randrange(100000, 1000000))
-        tm = str(int(time.time()))
-        params = ['apiKey=' + secret.cfpubkey, 'contestId=' + str(contestId), 'time=' + tm]
-        req = '/contest.standings?'
-        hs = rnd + req + '&'.join(params) + '#' + secret.cfsecretkey
-        hs = hs.encode('utf-8')
-        hs = str(hashlib.sha512(hs).hexdigest())
-        ref = "https://codeforces.com/api" + req + "&".join(params)
-        ref += '&apiSig=' + rnd + hs
-        response = requests.get(ref)
-        cfdata = get_data(response.json()['result']) 
+        id = request.form['contestId']
+        name = request.form['name']
+        contest = Contest(id, name)
+        db.session.add(contest)
+        db.session.commit()
 
     with open('include/index.html', 'r', encoding="utf-8") as page:
         data=page.read()
-    data = data.replace('%CF_DATA%', cfdata)
+    data = data.replace('%CONTESTS%', get_contest_table())
     return data 
