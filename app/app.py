ffec4d65d79fccba863c8351b5baad3445ce1fe4
Qu1s <trof1m00@mail.ru>
2019-12-02 01:59:41 +0300

    Added db for users and contests

--- app/app.py
+++ app/app.py
@@ -1,22 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+mysqlalchemy://Qu1s:@localhost/test1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.abspath('cf-challange.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 class Users(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    handle = db.Column(db.String(100), unique =  True)
+    handle = db.Column(db.String(50))
     name = db.Column(db.String(100))
 
-    def __init(self, handle, name):
+    def __init__(self, handle, name):
         self.handle = handle
         self.name = name
+
+class Contests(db.Model):
+    __tablename__ = 'contests'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), unique = True)
+
+    def __init__(self, name):
+        self.name = name
+
 
 
 
--- app/view.py
+++ app/view.py
@@ -1,12 +1,28 @@
 from app import *
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/users')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    count = db.session.query(Users).count()
+    return render_template('index.html', Contests = Contests, count = count)
+
+
+@app.route('/users', methods=['GET', 'POST'])
 def users():
-    return render_template('users.html')
+    
+    if request.method == "POST":
+
+        handle = request.form.get('handle')
+        name = request.form.get('name')
+
+        new_user = Users(handle, name)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('users', Users = Users))
+
+    return render_template('users.html', Users = Users)
+
 
 @app.route('/contest/')
 def contest():
